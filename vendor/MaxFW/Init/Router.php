0a8e9c34197744f5de5a39945969ceea6f526c5d
William Angélico <willangelico@gmail.com>
2017-09-11 11:04:35 -0300

    test admin route

--- vendor/MaxFW/Init/Router.php
+++ vendor/MaxFW/Init/Router.php
@@ -21,7 +21,8 @@ abstract class Router
         $array_url[2] = empty($array_url[2]) ? "index" : $array_url[2];
 
         array_walk($this->routes, function($route) use ($array_url) {
-          
+          var_dump($array_url);
+          var_dump($route);
            if($array_url[1] == $route['routes']){
               $array_url[3] = empty($array_url[3]) ? "index" : $array_url[3];
                $class       = "MaxBusiness\\".$route['folder']."\\Controllers\\".ucfirst($array_url[2]);
