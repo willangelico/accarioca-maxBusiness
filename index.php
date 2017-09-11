66b2403eca001d6dbd70e1536955e45d2ab598c2
William Angélico <willangelico@gmail.com>
2017-09-11 10:08:44 -0300

    change public files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MaxBusiness;
+
+require "../vendor/autoload.php"; 
+
+use MaxBusiness\Route;
+
+echo "index raiz";
+
+$route = new Route();
+
+// $testeClass = new teste();
+// $testeClass->testar();
+// echo "teste";
