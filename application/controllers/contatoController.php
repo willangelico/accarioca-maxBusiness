97e1d14070b03485e1aba118201b169e7e1aa966
William Angelico <William Angelico>
2017-09-12 22:28:26 -0300

    Fix - Contato e Orcamento Controllers

--- application/controllers/contatoController.php
+++ application/controllers/contatoController.php
@@ -5,10 +5,25 @@ class contatoController extends mainController
 	
 	public $login_required = false;
 
-    public function index() {
-		$modelo = $this->load_model('front/frontModel');
+   public function index() {
+
+		$modelo = $this->load_model('front/frontModel');		
+		$this->title = $modelo->config['titulo'];
+		$this->description = $modelo->config['meta_description'];
+        $this->page = 'contato';
+
+		$parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+
+        require ABSPATH .'/'. APPLICATION .'/views/helpers/header.php';
+        require ABSPATH .'/'. APPLICATION .'/views/helpers/nav.php';
+        require ABSPATH .'/'. APPLICATION .'/views/index/contato.php';
+        require ABSPATH .'/'. APPLICATION .'/views/helpers/footer.php';		
+    }   
+
+    public function send(){
+    	$modelo = $this->load_model('front/frontModel');
 		echo $modelo->envia_form($_POST);
     	return; 
-    } 
+    }
 	
 } 
--- application/controllers/orcamentoController.php
+++ application/controllers/orcamentoController.php
@@ -5,10 +5,24 @@ class orcamentoController extends mainController
 	
 	public $login_required = false;
 
-    public function index() {
-		$modelo = $this->load_model('front/frontModel');
+   public function index() {
+
+		$modelo = $this->load_model('front/frontModel');		
+		$this->title = $modelo->config['titulo'];
+		$this->description = $modelo->config['meta_description'];
+        $this->page = 'orcamento';
+
+		$parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+
+        require ABSPATH .'/'. APPLICATION .'/views/helpers/header.php';
+        require ABSPATH .'/'. APPLICATION .'/views/helpers/nav.php';
+        require ABSPATH .'/'. APPLICATION .'/views/index/orcamento.php';
+        require ABSPATH .'/'. APPLICATION .'/views/helpers/footer.php';		
+    }   
+	
+	public function send(){
+    	$modelo = $this->load_model('front/frontModel');
 		echo $modelo->envia_form($_POST);
     	return; 
-    } 
-	
+    }
 } 
