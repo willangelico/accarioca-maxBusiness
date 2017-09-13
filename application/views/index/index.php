e9c942091815247997bddcd9727b0eea6b85512c
William Angelico <willangelico@gmail.com>
2017-09-12 23:36:11 -0300

    hom links

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -2,13 +2,13 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-xs-12 col-sm-6">
-						<h2><a href="/materiais/casamento" title="Materiais para casamento - Faça sua Festa">
+						<h2><a href="/materiais" title="Materiais para casamento - Faça sua Festa">
 								<img src="/public/images/home-casamento.jpg" alt="Vai casar? Temos tudo para esta data especial!" />
 							</a>
 						</h2>
 					</div>
 					<div class="col-xs-12 col-sm-6">
-						<h2><a href="/materiais/aniversario" title="Materiais para aniversário - Faça sua Festa">
+						<h2><a href="/materiais" title="Materiais para aniversário - Faça sua Festa">
 								<img src="/public/images/home-aniversario.jpg" alt="Níver? É pic! É pic! É pic!" />
 							</a>
 						</h2>
