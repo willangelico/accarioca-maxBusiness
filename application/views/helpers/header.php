09c5af434394e1057d83c35c76d93ee763776976
William Angelico <willangelico@gmail.com>
2017-09-12 22:42:43 -0300

    Fix - CSS

--- application/views/helpers/header.php
+++ application/views/helpers/header.php
@@ -15,7 +15,7 @@
         <!-- Custom Styles -->  
         <link rel="stylesheet" href="/public/styles/style.css"> 
     </head>
-    <body id="home">
+    <body id="<?=$this->page;?>">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->  
